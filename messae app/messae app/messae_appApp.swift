79bf77f71d1bb62fcc43f54296689e567dd6d990
Mohd Adnan <mailtoadnan04@gmail.com>
2024-04-24 01:46:00 +0530

    This is a final Commit

---
+++ messae app/messae app/messae_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  messae_appApp.swift
+//  messae app
+//
+//  Created by Mohd Adnan on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct messae_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
